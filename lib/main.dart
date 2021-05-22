57aa77a81cc8849f9a2d902e862d6c43f8e5b094
kawarimidoll <kawarimidoll+git@gmail.com>
2021-05-22 10:29:57 +0900

    add message and colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Center(
-      child: Text('Hello World!'),
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('This is appBar!'),
+          backgroundColor: Colors.deepPurple[800],
+        ),
+        backgroundColor: Colors.teal[100],
+        body: Center(
+          child: Text('This is body!'),
+        ),
+      ),
     ),
-  ));
+  );
 }
